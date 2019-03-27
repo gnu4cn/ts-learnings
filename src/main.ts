721a40c3ca68bfe276b45e8b9e593603fc1ba7bc
gnu4cn <unisko@gmail.com>
2019-03-27 08:31:28 +0800

    Initial push.

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+'use strict';
+interface Lengthwise {
+    length: number;
+}
+
+function loggingIdentity<T extends Lengthwise>(arg: T): T {
+    console.log(arg.length); // 现在知道`arg`有着一个`.length`属性，因此不再报出错误
+    return arg;
+}
+
+loggingIdentity("test");
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
+}
+
+let x = { a: 1, b: 2, c: 3, d: 4 };
+
+console.log(getProperty(x, "a")); // 没有问题
+
+
+class BeeKeeper {
+    hasMask: boolean;
+}
+
+class ZooKeeper {
+    nametag: string;
+}
+
+class Animal {
+    numLegs: number;
+}
+
+class Bee extends Animal {
+    keeper: BeeKeeper;
+}
+
+class Lion extends Animal {
+    keeper: ZooKeeper;
+}
+
+function createInstance<A extends Animal>(c: new () => A): A {
+    return new c();
+}
+
+createInstance(Lion).keeper.nametag; // 
+createInstance(Bee).keeper.hasMask;
+
