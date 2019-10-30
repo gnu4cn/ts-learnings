a3e7b3692d8c97d2ebe5e1b1c512011a6c96f593
gnu4cn <unisko@gmail.com>
2019-10-30 11:07:30 +0800

    Updated.

---
+++ src/Triangle.ts
@@ -0,0 +1,8 @@
+/// <reference path="IShape.ts" />
+namespace Drawing {
+    export class Triangle implements IShape {
+        public draw(): void{
+            console.log("Triangle is drawn.");
+        }
+    }
+}
